7de405483ca207407addeb79b2df61e753f1c6e2
Bence Fábián <begnoc@gmail.com>
2020-03-04 13:49:32 +0100

    ec implemented

--- ec.ml
+++ ec.ml
@@ -1,8 +1,16 @@
+open Core
 open Shexp_process
 open Shexp_process.Let_syntax
 
+let files =
+  match Array.to_list Sys.argv with
+  | _ :: rest -> rest
+  | _ -> assert false
+
+
 let main: unit t =
-  let%bind tr = Util.command_exn "tr" in
-  echo "hello" |- tr ["h"; "H"]
+  let%bind emacs_client = Util.command_exn "emacsclient" in
+  let open Base in
+  emacs_client (List.append ["-ca"; ""] files)
 
 let () = Util.wrap_main main
