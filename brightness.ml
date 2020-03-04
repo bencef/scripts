4c8afe89c50700e3621084f6394e4c4d80877575
Bence Fábián <begnoc@gmail.com>
2020-03-04 16:33:15 +0100

    read amount

--- brightness.ml
+++ brightness.ml
@@ -1,15 +1,26 @@
 open Core
 open Shexp_process
 
+(* todo: make this overridable from the environment *)
+let brightness_file = "/sys/class/backlight/intel_backlight/brightness"
+
 type args =
   | Read
   | Set of int
   | Error of string
 
+let read_amount amount =
+  match int_of_string_opt amount with
+  | None -> Error amount
+  | Some number ->
+     if number <= 900 && number >= 50
+     then Set number
+     else Error (Printf.sprintf "%d is out of range [50..900]" number)
+
 let parse_args () =
   match Array.length Sys.argv with
   | 1 -> Read
-  | 2 -> Set 500
+  | 2 -> read_amount (Array.get Sys.argv 1)
   | _ -> Error "Too many arguments"
 
 let read_brightness () =
